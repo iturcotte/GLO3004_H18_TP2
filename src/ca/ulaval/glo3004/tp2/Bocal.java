473155626d7f390ac74298065c610b94b8b2dfdf
Ian Turcotte <ianturcotte@hotmail.com>
2018-03-19 21:00:51 -0400

    Initial commit

---
+++ src/ca/ulaval/glo3004/tp2/Bocal.java
@@ -0,0 +1,61 @@
+package ca.ulaval.glo3004.tp2;
+
+public class Bocal extends Thread {
+
+	
+	
+	
+	
+	@Override
+	public void run() {
+		
+	}
+	
+	
+	public void requeteValve() {
+		
+	}
+	
+	public void ouvreValve() {
+		
+	}
+	
+	
+	public void remplit() {
+		
+	} 
+
+	
+	public void fermeValve() {
+		
+	}
+	
+	
+	
+	public void rupture() {
+		
+	} 
+
+	
+	public void finRupture() {
+		
+	}
+	
+	
+	public void requeteEtiquetage() {
+		
+	} 
+
+	public void commenceEtiquetage() {
+		
+	} 
+
+	public void etiquette() {
+		
+	} 
+
+	public void termineEtiquetage() {
+		
+	}
+	
+}
---
+++ src/ca/ulaval/glo3004/tp2/Confiturerie.java
@@ -0,0 +1,12 @@
+package ca.ulaval.glo3004.tp2;
+
+public class Confiturerie {
+
+	
+	
+	
+	
+	
+	
+	
+}
